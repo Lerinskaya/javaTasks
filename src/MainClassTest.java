cbf0fadaa63cfd8490e7a97005b26e851ff0f08f
Lerinskaya <rammnescence1@gmail.com>
2024-07-01 13:16:38 +0200

    add second task

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -9,4 +9,12 @@ public class MainClassTest {
         int actualResult = MainClass.getLocalNumber();
         Assert.assertEquals("Expected result is not 14", expectedResult, actualResult);
     }
+
+    @Test
+    public void testGetClassNumber(){
+        MainClass mainClass = new MainClass();
+        int controlNumber = 45;
+        int actualNumber = mainClass.getClassNumber();
+        Assert.assertTrue("Class number should be greater than " + controlNumber,actualNumber>controlNumber);
+    }
 }
