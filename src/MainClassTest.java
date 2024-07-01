be39623b76b8a6b588bc192c9de9d865fc408b1b
Lerinskaya <rammnescence1@gmail.com>
2024-07-01 13:40:52 +0200

    remove class object

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 public class MainClassTest {
     @Test
     public void testGetLocalNumber(){
-        MainClass mainClass = new MainClass();
         int expectedResult = 14;
         int actualResult = MainClass.getLocalNumber();
         Assert.assertEquals("Expected result is not 14", expectedResult, actualResult);
